7b80d60dbf121bd271354fb922617a8c3fee4d8e
Bryce Nichol <bryce.n.dev@icloud.com>
2023-01-21 14:46:08 -0500

    Select random cat photo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,44 @@
 use cli_clipboard;
-
+use colored::Colorize;
+use rand::Rng;
 
 fn main() {
-    let the_string = "";
-    cli_clipboard::set_contents(the_string.to_owned()).unwrap();
-    println!("🐈 Success! Cat photo URL successfully copied to clipboard.");
+    let photo_names = [
+        "cat_bunk_beds.jpg",
+        "cat_hens.jpg",
+        "cat_snuggles_napping.jpg",
+        "cat_car_sleep.jpg",
+        "cat_lot_on_my_mlnd.jpg",
+        "cat_star.jpg",
+        "cat_cards.png",
+        "cat_money.png",
+        "cat_sunglasses.jpg",
+        "cat_cartoon_bomb.jpg",
+        "cat_pope.jpg",
+        "crow.jpg",
+        "cat_cowboy.jpg",
+        "cat_punch.jpg",
+        "dog.jpg",
+        "cat_cowboy_scream.png",
+        "cat_scream.png",
+        "duck.png",
+        "cat_eyes.png",
+        "cat_scream_floor.jpg",
+        "cat_gang.jpg",
+        "cat_showering.png",
+    ];
+
+    let mut rng = rand::thread_rng();
+
+    let photo_name = photo_names[rng.gen_range(0..photo_names.len())];
+    let url = format!(
+        "https://raw.githubusercontent.com/bryce-n-dev/gato/main/assets/cats/{}",
+        photo_name
+    );
+
+    cli_clipboard::set_contents(url.to_owned()).expect("Oh no! Something went wrong.");
+    println!(
+        "🐈 {} Cat photo URL successfully copied to clipboard.",
+        "Success!".green()
+    );
 }
